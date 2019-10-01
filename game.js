61689bec3979ff7d08f52bf003cf62229ae6810e
Prashant Sharma <sprashant.30@gmail.com>
2019-10-01 16:15:21 +0530

    paddle and bounce on paddle done

--- game.js
+++ game.js
@@ -7,17 +7,40 @@ const PADDLE_WIDTH = 100;
 const PADDLE_THICKNESS = 10;
 var paddleX = 400;
 
+function updateMousePos(event) {
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+
+
+  var mouseX = event.clientX - rect.left - root.scrollLeft;
+  //var mouseY = event.clientY - rect.top - root.scrollTop;
+
+  paddleX = mouseX;
+}
+
 window.onload = function onload() {
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
 
   var framesperSecond = 30;
   setInterval(updateAll, 1000 / framesperSecond);
+
+  canvas.addEventListener('mousemove', updateMousePos);
+}
+
+function resetCanvas() {
+  ballX = 0;
+  ballY = 0;
 }
 
 function moveAll() {
   ballX += BallSpeedX;
   ballY += BallSpeedY;
+
+  // if ball y is 0 and x is not in range of paddle restart
+  if (ballY === canvas.height && (ballX < paddleX || ballX > paddleX + 100)) {
+    return resetCanvas();
+  }
 
   if (ballX > canvas.width || ballX < 0) {
     BallSpeedX *= -1
@@ -43,6 +66,7 @@ function colorCircle(centerX, centerY, radius, fillColor) {
 function drawAll() {
   colorRect(0, 0, canvas.width, canvas.height, 'black');
   colorCircle(ballX, ballY, 10, 'white');
+  colorRect(paddleX, canvas.height - PADDLE_THICKNESS, PADDLE_WIDTH, PADDLE_THICKNESS, 'white');
 }
 
 function updateAll() {
